ce0bc5e59bbcb941eb0e00cced1787eb295d322e
foxycrafter <85259021+foxycrafter@users.noreply.github.com>
2023-05-22 16:50:26 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
 			"max-video-preview": -1,
 			"max-image-preview": "large",
 			"max-snippet": -1,
-		}
+		},
 	twitter: {
 		title: "Miguel
 		card: "summary_large_image",
