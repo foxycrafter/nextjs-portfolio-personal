d0a270a96938ba5722b5849d24cdf2c8e7c95356
foxycrafter <85259021+foxycrafter@users.noreply.github.com>
2023-05-22 17:10:22 +0200

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -35,6 +35,12 @@ export const Navigation: React.FC = () => {
 						>
 							Contact
 						</Link>
+						<Link
+							href="/skills"
+							className="duration-200 text-zinc-400 hover:text-zinc-100"
+						>
+							Skills
+						</Link>
 					</div>
 
 					<Link
