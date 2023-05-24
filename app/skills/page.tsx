f5d31dc67b719292b8741f789f72e2e4dec864b6
foxycrafter <85259021+foxycrafter@users.noreply.github.com>
2023-05-24 13:56:38 -0500

    :v

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -7,65 +7,62 @@ import { Article } from "./article";
 import { Redis } from "@upstash/redis";
 import { Eye } from "lucide-react";
 import { SiPython, SiReact } from "react-icons/si";
-
+import styles from "./importd.css"
 export const revalidate = 60;
 export default async function ProjectsPage() {
   return (
-    <div className="relative pb-16">
+    <div className={styles.container}>
       <Navigation />
-      <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32 text-align:center justify-center items-center h-screen">
-        <div className="max-w-2xl mx-auto lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-            SkillSets
-          </h2>
-        </div>
-        <div className="w-full h-px bg-zinc-800" />
-        <p className="mt-4 text-zinc-400">Languages.</p>
-        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
-          <Card>
-            <p className="mt-4 text-zinc-400">English = B2+ = Toic / Tofel / Opp</p>
-            <p className="mt-4 text-zinc-400">Spanish = Native</p>
+      <div className={styles.container}>
+        <h2 className={styles.title}>SkillSets</h2>
+        <div className={styles.divider} />
+        <p className={styles.card-content}>Languages.</p>
+        <div className={styles.card-grid}>
+          <Card className={styles.card}>
+            <p className={styles.card-content}>English = B2+ = Toic / Tofel / Opp</p>
+            <p className={styles.card-content}>Spanish = Native</p>
           </Card>
         </div>
-        <div className="w-full h-px bg-zinc-800" />
-        <p className="mt-4 text-zinc-400">Programming.</p>
-        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
-          <Card>
-            <p className="mt-4 text-zinc-400">Python = Intermediate</p>
-            <p className="mt-4 text-zinc-400">Nextjs = Intermediate</p>
-            <p className="mt-4 text-zinc-400">React = Intermediate</p>
-            <p className="mt-4 text-zinc-400">Bootstrap = Intermediate</p>
+        <div className={styles.divider} />
+        <p className={styles.card-content}>Programming.</p>
+        <div className={styles.card-grid}>
+          <Card className={styles.card}>
+            <p className={styles.card-content}>Python = Intermediate</p>
+            <p className={styles.card-content}>Nextjs = Intermediate</p>
+            <p className={styles.card-content}>React = Intermediate</p>
+            <p className={styles.card-content}>Bootstrap = Intermediate</p>
           </Card>
         </div>
-        <div className="hidden w-full h-px md:block bg-zinc-800" />
-        <p className="mt-4 text-zinc-400">Design and Modeling</p>
-        <div className="grid grid-cols-1 gap-8 mx-auto lg:mx-0 md:grid-cols-2">
-          <Card>
-            <p className="mt-4 text-zinc-400">Blender = Intermediate</p>
-            <p className="mt-4 text-zinc-400">Krita = Intermediate</p>
-            <p className="mt-4 text-zinc-400">Substance = Intermediate</p>
-            <p className="mt-4 text-zinc-400">Photoshop = Basic / Intermediate</p>
+        <div className={styles.hidden-divider} />
+        <p className={styles.card-content}>Design and Modeling</p>
+        <div className={styles.card-grid}>
+          <Card className={styles.card}>
+            <p className={styles.card-content}>Blender = Intermediate</p>
+            <p className={styles.card-content}>Krita = Intermediate</p>
+            <p className={styles.card-content}>Substance = Intermediate</p>
+            <p className={styles.card-content}>Photoshop = Basic / Intermediate</p>
           </Card>
         </div>
-        <div className="hidden w-full h-px md:block bg-zinc-800" />
-        <p className="mt-4 text-zinc-400">Database.</p>
-        <div className="grid  grid-cols-1 gap-8 mx-auto lg:mx-0 md:grid-cols-2">
-          <Card>
-            <p className="mt-4 text-zinc-400">Mysql = Intermediate</p>
-            <p className="mt-4 text-zinc-400">Postgrest = Intermediate</p>
+        <div className={styles.hidden-divider} />
+        <p className={styles.card-content}>Database.</p>
+        <div className={styles.card-grid}>
+          <Card className={styles.card}>
+            <p className={styles.card-content}>Mysql = Intermediate</p>
+            <p className={styles.card-content}>Postgrest = Intermediate</p>
           </Card>
         </div>
-        <div className="hidden w-full h-px md:block bg-zinc-800" />
-        <p className="mt-4 text-zinc-400">General knowledge</p>
-        <div className="grid  grid-cols-1 gap-8 mx-auto lg:mx-0 md:grid-cols-2 ">
-          <Card>
-            <p className="mt-4 text-zinc-400">Virtual machines = Intermediate</p>
-            <p className="mt-4 text-zinc-400">Cybersecurity Introduction = Intermediate</p>
-            <p className="mt-4 text-zinc-400">Linux Distributions = Basic / Intermediate</p>
-            <p className="mt-4 text-zinc-400">Cybersecurity with OSINT = Basic / Intermediate</p>
+        <div className={styles.hidden-divider} />
+        <p className={styles.card-content}>General knowledge</p>
+        <div className={styles.card-grid}>
+          <Card className={styles.card}>
+            <p className={styles.card-content}>Virtual machines = Intermediate</p>
+            <p className={styles.card-content}>Cybersecurity Introduction = Intermediate</p>
+            <p className={styles.card-content}>Linux Distributions = Basic / Intermediate</p>
+            <p className={styles.card-content}>Cybersecurity with OSINT = Basic / Intermediate</p>
           </Card>
         </div>
       </div>
     </div>
   );
 }
+
